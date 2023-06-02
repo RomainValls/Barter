365995823dde79dda17d5d02e6c277ebffa0952f
Bryan Tsanga <bryan-t@hotmail.fr>
2023-06-02 09:37:25 +0200

    did some stuff

--- backend/models/Skills.model.js
+++ backend/models/Skills.model.js
@@ -1,8 +1,23 @@
 const { Schema, model } = require("mongoose");
 
 const skillsSchema = new Schema({
-  name: { type: String, required: true },
-  serviceCategory: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    serviceCategory: {
+      type: String,
+      required: true,
+      enum: [
+        "Personal",
+        "Professional",
+        "Health and Wellness",
+        "Educational",
+        "Creative",
+        "Home",
+        "Transportation",
+      ],
+    },
+  },
 });
 
 const Skills = model("Skills", skillsSchema);
