6e5bfb8275504de7692604ed00af6f95533b484a
Romain Valls <romain.valls95@gmail.com>
2023-06-07 15:00:34 +0200

    worked on the nodemailer stuff. need to send to the right person now

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,17 +1,45 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+const express = require("express");
+
+// WEBSOCKET
+
+// const { createServer } = require("http");
+
+// const { Server } = require("socket.io");
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+// io.attach(server);
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI = process.env.MONGODB_URI;
 
+const PORT = process.env.PORT || 5005;
+
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
     const dbName = x.connections[0].name;
+
     console.log(`Connected to Mongo! Database name: "${dbName}"`);
+
+    // const collectionName = "requests"; // Replace with the actual name of your collection
+
+    // const collection = mongoose.connection.collection(collectionName);
+
+    // const changeStream = collection.watch();
+
+    // changeStream.on("change", (change) => {
+    //   console.log("Change:", change);
+    //   console.log("CONSOLE LOG DU CHANGE STREAM");
+    //   // Show notification using react-toastify
+    //   io.emit("databaseChange");
+    // });
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
